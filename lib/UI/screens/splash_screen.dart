88752401a37a8926afbc2e41691de535d63caecb
Kamilo Chinome <kamiloandresch@gmail.com>
2023-11-20 11:07:07 -0500

    splash screen finished

--- lib/UI/screens/splash_screen.dart
+++ lib/UI/screens/splash_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:lottie/lottie.dart';
 import 'package:sizer/sizer.dart';
 import 'package:technical_test/config/constants/colors.dart';
@@ -11,31 +12,72 @@ class SplashScreen extends StatefulWidget {
   State<SplashScreen> createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen> {
+class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 2500),
+    );
+    _animationController.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        _animationController.reverse();
+      } else if (status == AnimationStatus.dismissed) {
+        context.pushReplacementNamed('home');
+      }
+    });
+    _animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
+    TextTheme textStyle = Theme.of(context).textTheme;
+
     return Scaffold(
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.end,
         children: [
-          LottieBuilder.asset(
-            'assets/animations/LottieAnimation.json',
-            height: 90.w,
-            repeat: true,
-            reverse: true,
-            fit: BoxFit.cover,
-          ),
           Container(
-            height: 40.h,
+            height: 80.h,
             width: double.infinity,
             decoration: BoxDecoration(
               color: colorPrimaryContainer,
               borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(15.w),
-                topRight: Radius.circular(15.w),
+                bottomLeft: Radius.circular(37.5.h),
+                bottomRight: Radius.circular(37.5.h),
               ),
             ),
-          )
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                LottieBuilder.asset(
+                  'assets/animations/LottieAnimation.json',
+                  controller: _animationController,
+                  animate: false,
+                  frameRate: FrameRate.max,
+                  repeat: true,
+                  reverse: true,
+                  fit: BoxFit.cover,
+                ),
+                Text(
+                  'Kamilo Chinome',
+                  style: textStyle.titleLarge,
+                ),
+                Text(
+                  'Desarrollador de Aplicaciones Móviles',
+                  style: textStyle.titleMedium,
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
--- lib/config/theme/app_theme.dart
+++ lib/config/theme/app_theme.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:sizer/sizer.dart';
 import 'package:technical_test/config/constants/colors.dart';
 
 class AppTheme {
-  static lightTheme() => ThemeData(useMaterial3: true, scaffoldBackgroundColor: colorBackground);
+  static lightTheme() => ThemeData(
+        useMaterial3: true,
+        scaffoldBackgroundColor: colorBackground,
+        textTheme: TextTheme(
+          titleLarge: TextStyle(fontSize: 20.sp, fontWeight: FontWeight.bold),
+        ),
+      );
 }
